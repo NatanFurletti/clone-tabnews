49f04d26a0eb930e7290e2629a980482859c20e2
Natan Furletti <128628722+NatanFurletti@users.noreply.github.com>
2026-09-03 02:09:31 +0000

    chore: add connections info

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,31 @@
 import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result);
-  res.status(200).send("test");
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionVersion = databaseVersionResult.rows[0].server_version;
+
+  const maxConnectionsResult = await database.query("SHOW max_connections");
+  const maxConnections = maxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const openedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const openedConnectionsValue = openedConnectionsResult.rows[0].count;
+
+  res.status(200).send({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionVersion,
+        max_connections: parseInt(maxConnections),
+        opened_connections: openedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
