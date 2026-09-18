3033115ceabb7de0ac2e55d4f7c9b5e3b86d86e2
Natan Furletti <128628722+NatanFurletti@users.noreply.github.com>
2026-09-18 23:25:50 +0000

    fix: catch error

--- infra/database.js
+++ infra/database.js
@@ -12,7 +12,7 @@ async function query(queryObject) {
     await client.connect();
     const result = await client.query(queryObject);
     return result;
-  } catch {
+  } catch (error) {
     console.error(error);
     throw error;
   } finally {
