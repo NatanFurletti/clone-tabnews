fe28193a6f39f0b9e2887429f3de160a4f0abb06
Natan Furletti <128628722+NatanFurletti@users.noreply.github.com>
2026-09-22 04:11:38 +0000

    refactor: index.js

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,28 +3,29 @@ import database from "infra/database.js";
 async function status(req, res) {
   const updatedAt = new Date().toISOString();
 
+  const databaseNameResult = await database.query("SELECT current_database();");
+  const databaseName = databaseNameResult.rows[0].current_database;
+
   const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersion = databaseVersionResult.rows[0].server_version;
 
   const maxConnectionsResult = await database.query("SHOW max_connections;");
   const maxConnections = maxConnectionsResult.rows[0].max_connections;
 
-  const databaseName = process.env.POSTGRES_DB;
   const openedConnectionsResult = await database.query({
     text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
     values: [databaseName],
   });
+  const openedConnections = openedConnectionsResult.rows[0].count;
 
-  // rows count --> length do dado???
-  const openedConnectionsValue = openedConnectionsResult.rows[0].count;
-
-  res.status(200).send({
+  res.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
+        name: databaseName,
         version: databaseVersion,
         max_connections: parseInt(maxConnections),
-        opened_connections: openedConnectionsValue,
+        opened_connections: openedConnections,
       },
     },
   });
